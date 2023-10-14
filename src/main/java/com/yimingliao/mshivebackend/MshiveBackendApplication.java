afd6b950dc94d6176f0d56f2ba7de9fb61c71f33
Hooorus <2479711422@qq.com>
2023-10-14 16:49:35 +0800

    init

---
+++ src/main/java/com/yimingliao/mshivebackend/MshiveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yimingliao.mshivebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MshiveBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MshiveBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yimingliao/mshivebackend/MshiveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yimingliao.mshivebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MshiveBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
