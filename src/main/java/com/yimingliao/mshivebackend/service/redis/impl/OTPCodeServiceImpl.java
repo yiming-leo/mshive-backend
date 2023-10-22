9eae1b556da63df8ce97e98222217d5788e08d2e
Hooorus <2479711422@qq.com>
2023-10-22 12:45:49 +0800

    add duration defender for defending OTP Code requesting frequently

--- src/main/java/com/yimingliao/mshivebackend/service/redis/impl/OTPCodeServiceImpl.java
+++ src/main/java/com/yimingliao/mshivebackend/service/redis/impl/OTPCodeServiceImpl.java
@@ -37,22 +37,28 @@ public class OTPCodeServiceImpl implements IOTPCodeService {
 
     @Override
     public R OTPCodeSenderByDynamicHtmlEmail(String userHttpIp, Integer length,
-                                      Integer duration, String from, String to, String htmlFilePath)
+                                             Integer duration, String from, String to, String htmlFilePath)
             throws MessagingException, IOException {
-        String code = otpCode.generateOTPCode(length);
-        String otpCode = emailSendUtil.sendOTPDynamicHtmlMail(code, duration, from, to, htmlFilePath);
-        //inject validate code (key-value pair) to redis set 10min
-        //code==otpCode防止被注入
-        if (!Objects.equals(code, otpCode)) {
-            return R.success(404, "Something wrong, try again", new Date());
+        //limit validation request time > 60s || no validation request time
+        if ((redisTemplate.opsForValue().get(userHttpIp) == null) || (redisTemplate.getExpire(userHttpIp) <= 60)) {
+            log.info("opt-value: " + redisTemplate.opsForValue().get(userHttpIp));
+            log.info("ttl time: " + redisTemplate.getExpire(userHttpIp));
+            String code = otpCode.generateOTPCode(length);
+            String otpCode = emailSendUtil.sendOTPDynamicHtmlMail(code, duration, from, to, htmlFilePath);
+            //inject validate code (key-value pair) to redis set 10min
+            //code==otpCode防止被注入
+            if (!Objects.equals(code, otpCode)) {
+                return R.success(404, "Something wrong, try again", new Date());
+            }
+            redisTemplate.opsForValue().set(userHttpIp, otpCode, duration, TimeUnit.MINUTES);
+            return R.success(200, "Code Sent", new Date());
+        } else {
+            return R.error(403, "Try Again Later!", new Date());
         }
-        redisTemplate.opsForValue().set(userHttpIp, otpCode, duration, TimeUnit.MINUTES);
-        return R.success(200, "Code Sent", new Date());
     }
 
     @Override
     public R OTPCodeValidate(String userHttpIp, String userOTP) throws Exception {
-        //Insert User IP and Return Value
         String RedisValue = redisTemplate.opsForValue().get(userHttpIp);
         //validate RedisValue & userOTP
         if ((userOTP != null && RedisValue != null) && userOTP.equals(RedisValue)) {
@@ -60,5 +66,6 @@ public class OTPCodeServiceImpl implements IOTPCodeService {
         } else {
             return R.error(404, "Validate Failed!", new Date());
         }
+
     }
 }
