6cde16ec2e8f93b80797343b1989e64b4a99ff79
Hooorus <2479711422@qq.com>
2023-10-28 12:35:42 +0800

    finish Furniture Service download(response) Furniture file

--- src/main/java/com/yimingliao/mshivebackend/controller/version1/furniture/FurnitureController.java
+++ src/main/java/com/yimingliao/mshivebackend/controller/version1/furniture/FurnitureController.java
@@ -3,9 +3,14 @@ package com.yimingliao.mshivebackend.controller.version1.furniture;
 import com.yimingliao.mshivebackend.common.R;
 import com.yimingliao.mshivebackend.entity.mongodb.Furniture;
 import com.yimingliao.mshivebackend.service.mongodb.impl.FurnitureServiceImpl;
+import com.yimingliao.mshivebackend.service.mongodb.impl.UserServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
 
 /**
  * @author Calendo
@@ -20,6 +25,9 @@ public class FurnitureController {
 
     @Autowired
     private FurnitureServiceImpl furnitureService;
+
+    @Autowired
+    private UserServiceImpl userService;
 
     //Insert One Furniture
     @PutMapping("/insert_one")
@@ -58,5 +66,25 @@ public class FurnitureController {
     public R searchOneFurnitureByUserUUId(@RequestParam(name = "userUUId") String userUUId,
                                      @RequestParam(name = "furnitureId") Long furnitureId) {
         return furnitureService.searchOneFurnitureByUserUUId(userUUId, furnitureId);
+    }
+
+    //Download One User's Optional Rom Report Form, need userUUId & JSON:RoomReportForm
+    @PostMapping("/{user_uuid}/download_report_form")
+    public R downloadOneUserRoomReportForm(HttpServletResponse response,
+                                           @PathVariable("user_uuid") String userUUId,
+                                           @RequestParam(name = "start_date") String startDate,
+                                           @RequestParam(name = "end_date") String endDate,
+                                           @RequestParam(name = "only_bookmarks") Boolean onlyBookmarks,
+                                           @RequestParam(name = "need_all") Boolean needAll
+    ) {
+        if (userService.searchOneUserByUserUUId(userUUId).getStatus() != 200) {
+            return R.error(403, "Download Forbidden", new Date(), "无权限下载");
+        }
+        //roomReportForm
+        try {
+            return furnitureService.downloadOneUserFurnitureReportForm(response, userUUId, startDate, endDate, onlyBookmarks, needAll);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
